8ec32745e4799494ce91a603b1bfcb513b2c4e3a
juansgc11-3 <yakelinepelaez98@gmail.com>
2025-07-30 16:45:12 -0500

    dia de media tecnica y full

--- carrusel.js
+++ carrusel.js
@@ -34,3 +34,26 @@ setInterval(() => {
     index = (index + 1) % total;
     actualizarCarrusel();
 }, 4000);
+
+document.addEventListener("DOMContentLoaded", () => {
+    const botones = document.querySelectorAll(".boton-menu");
+    const contenedores = document.querySelectorAll(".contenido");
+  
+    botones.forEach((boton, index) => {
+      boton.addEventListener("click", () => {
+        const contenidoActual = contenedores[index];
+        const visible = contenidoActual.style.display === "block";
+  
+        // Cierra todos los contenidos
+        contenedores.forEach((contenido) => {
+          contenido.style.display = "none";
+        });
+  
+        // Solo abre el actual si estaba cerrado
+        if (!visible) {
+          contenidoActual.style.display = "block";
+        }
+      });
+    });
+  });
+  
